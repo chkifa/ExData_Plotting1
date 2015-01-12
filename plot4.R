c2e8479248a24fd7ccb347e364c06c38d44491c7
chkifa <chkifak@gmail.com>
2015-01-12 00:02:28 +0000

    last commit

--- plot4.R
+++ plot4.R
@@ -19,13 +19,19 @@ is.numeric(HH_power_P1$Sub_metering_2)
 HH_power_P1$Sub_metering_3 <- as.numeric(HH_power_P1$Sub_metering_3)
 is.numeric(HH_power_P1$Sub_metering_3)
 
-par(mfrow=c(1,1))
+par(mfrow=c(2,2))
+##plot1:1.1
+plot(HH_power_P1_Date,HH_power_P1$Global_active_power, type="l",xlab="",ylab="Global active power")
+##plot2:1.2
+plot(HH_power_P1_Date,HH_power_P1$Voltage, type="l",xlab="datetime",ylab="Voltage")
+##plot3:2.1
 plot(HH_power_P1_Date,HH_power_P1$Sub_metering_1, type="n",xlab="",ylab="Energy sub metering")
 lines(HH_power_P1_Date,HH_power_P1$Sub_metering_1, col="black")
 lines(HH_power_P1_Date,HH_power_P1$Sub_metering_2, col="red")
 lines(HH_power_P1_Date,HH_power_P1$Sub_metering_3, col="blue")
 legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
-       pch="-",lwd=3,col=c("black", "red", "blue"), bty="l")
-	   
-dev.copy(png,file=".\\GitHub\\ExData_Plotting1\\Plot3.png",width = 480, height = 480)     
+       pch="-",lwd=3,col=c("black", "red", "blue"), bty="n",cex=0.7)
+##plot4:2.2
+plot(HH_power_P1_Date,HH_power_P1$Global_reactive_power,type="l",xlab="datetime",ylab="Global_reactive_power")
+dev.copy(png,file=".\\GitHub\\ExData_Plotting1\\Plot4.png",width = 480, height = 480)     
 dev.off()
